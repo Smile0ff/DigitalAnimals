76972b7c2c9f9b941f875736e4ec383f588fe6a1
Smile0ff <a.parneta@gmail.com>
2016-02-25 20:22:13 +0200

    mobile

--- assets/js/app/home.js
+++ assets/js/app/home.js
@@ -1,12 +1,19 @@
 "use strict";
 
 import "jquery";
+import isMobile from "../lib/isMobile";
 import Scrollify from "../lib/scrollify";
 import Parallaxify from "../lib/parallaxify";
+import Loader from "../lib/loader";
+
+new Loader();
 
 $(() => {
+
     new Scrollify();
     new Parallaxify();
+    
 });
+
 
 $(window).scrollTop(0);
---
+++ assets/js/lib/loader.js
@@ -0,0 +1,22 @@
+"use strict";
+
+let body = $("body");
+
+class Loader{
+
+    constructor(){
+        this._events();
+    }
+    _events(){
+        $(window).on("load", (e) => { this._handleLoad(e) });
+    }
+    _handleLoad(e){
+
+        window.setTimeout(() => {
+            body.addClass("__loaded");
+        }, 2000);
+    }
+
+}
+
+export default Loader;
